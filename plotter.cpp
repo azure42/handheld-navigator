0e297237338834579b304ed10a32662da7d4f87c
ljy <liu.jingyang@foxmail.com>
2016-01-23 03:18:00 +0800

    光标定位完成

--- plotter.cpp
+++ plotter.cpp
@@ -16,28 +16,22 @@ extern class readKey *readKeyThread;
 Plotter::Plotter(QWidget *parent) :
     QWidget(parent)
 {
-    //    QObject::connect(readKeyThread,SIGNAL(dirKey(int)),plView, SLOT(dirGet(int)));//方向键
+    QTimer *rTimer = new QTimer(this);
+    rTimer->start(200);
+    connect(rTimer,SIGNAL(timeout()),this,SLOT(showTime()));
+
+    QObject::connect(beidouData,SIGNAL(dataUpdate()),this,SLOT(showTime()));
 
     scene = new QGraphicsScene;
-    scene->setSceneRect(0, 0, 4000, 3000);
+    scene->setSceneRect(0, 0, viewWidth, viewHeight);
     plView =new PlView;
     plView->setScene(scene);
-    plView->setRenderHint(QPainter::Antialiasing);
-    //    const QPoint zero = QPoint(plView->width())
-
-    QGraphicsItem *zero = new QGraphicsEllipseItem(
-                QRectF(plView->width()/2.0+150,
-                       plView->height()/2.0+50,
-                       10,10));
-    scene->addItem(zero);
-
-    //    scene->addEllipse(QRectF(plView->width()/2.0+150,
-    //                             plView->height()/2.0+50,
-    //                             10,10));//用圆形表示当前位置
-
-    //   plView->centerOn(QPoint(plView->width()/2,plView->height()/2));
+    plView->setRenderHint(QPainter::Antialiasing);//抗锯齿
     plView->centerOn(QPoint(0,0));
-    //    plView->scroll(0,0);
+    plView->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    //默认视野在中间,调至左上角否则无法实现鼠标全局坐标和view相对坐标的对应
+
+    //窗口静态布局初始化
     {
         settingLabel =new QLabel(tr("SETTING"));
         settingLabel->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
@@ -49,7 +43,7 @@ Plotter::Plotter(QWidget *parent) :
 
         SPDLabel =new QLabel(tr("SPD"));
         SPDLabel->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-        SPDLabel->setFrameShape (QFrame::Box);
+        SPDLabel->setFrameShape (QFrame::Panel);
 
         coordinateLabel =new QLabel(tr("longitude\t\tlatitude"));
         coordinateLabel->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
@@ -67,10 +61,23 @@ Plotter::Plotter(QWidget *parent) :
         mainLayout->setColumnStretch(0,1);
         mainLayout->setColumnStretch(1,5);
     }
+
+    //创建一个直径8像素的圆形item代表当前位置
+    QGraphicsItem *zero = new QGraphicsEllipseItem(
+                QRectF(viewWidth/2.0,viewHeight/2.0,8,8));
+//    plView->horizontalScrollBar()->setHidden(true);
+//    plView->verticalScrollBar()->setHidden(true);
+
+//    QPoint *temp = new QPoint(plView->mapToGlobal(QPoint(viewWidth/2,viewHeight/2)).x(),
+//                              plView->mapToGlobal(QPoint(viewWidth/2,viewHeight/2)).y());
+    plView->cursor.setPos(viewWidth/2+10*3+viewWidth/5+1,viewHeight/2+10*2+4);
+    scene->addItem(zero);
+    qDebug("%lf,%lf",zero->x(),zero->y());
+
 }
 
 void Plotter::showTime()
-{
+{//文字刷新
     CSEString = "CSE:";
     CSEString.append(beidouData->cog);
     CSEString.append("°");
@@ -81,8 +88,9 @@ void Plotter::showTime()
     SPDString.append("knot");
     SPDLabel->setText(SPDString);
 
+       const QPointF nowCoor =plView->coorCalc(cursor().pos(),viewWidth,viewHeight);
+
     coordinateString = "Longitude:";
-    const QPointF nowCoor =plView->coorCalc( mapFromGlobal(cursor().pos()));
     coordinateString.append(QString::number(nowCoor.x()));
     if(beidouData->E.toInt())//东南西北后缀判别
         coordinateString.append(QString("E\nLatitude:"));
@@ -97,17 +105,19 @@ void Plotter::showTime()
 }
 
 /*****************
-*
-* 输入：屏幕坐标
+* coorCalc
+* 输入：光标位置
 * 输出：经纬度
 *
 *****************/
-QPointF PlView::coorCalc(QPoint nowPos)
+QPointF PlView::coorCalc(QPoint nowPos,int viewWidth,int viewHeight)
 {
-    return QPointF(beidouData->Lon.toInt()+(nowPos.x()-zero->pos().x())*BEISHU,
-                   beidouData->Lat.toInt()+(nowPos.y()-zero->pos().y())*BEISHU
+    nowPos = mapFromGlobal(nowPos);
+    return QPointF(beidouData->Lon.toInt()+(nowPos.x()-viewWidth/2.0)*BEISHU,
+                   beidouData->Lat.toInt()+(nowPos.y()-viewHeight/2.0)*BEISHU
                    );
 }
+
 
 /*****************
 *
@@ -152,47 +162,23 @@ PlView::PlView(QWidget *parent) :
     QCursor cursor ;
     cursor = QCursor(Qt::CrossCursor);
     setCursor(cursor);
-    cursor.setPos(QPoint(500,400));
+//    cursor.setPos(QPoint(500,400));
 }
-/*
-void PlView::dirGet(int num)
-{
-    switch (num)
-    {
-    case 1 ://上
-        cursor.setPos((QPoint(cursor.pos().x(),cursor.pos().y()-10)));
-        break;
-    case 2 ://下
-        cursor.setPos((QPoint(cursor.pos().x(),cursor.pos().y()+10)));
-        break;
-    case 3 ://左
-        cursor.setPos((QPoint(cursor.pos().x()-10,cursor.pos().y())));
-        break;
-    case 4 ://右
-        cursor.setPos((QPoint(cursor.pos().x()+10,cursor.pos().y())));
-        break;
-        //     case 5://确认键，添加点或删除选中点
 
-    case Qt::Key_Equal :
-        scale(1.2, 1.2);
-        break;
-    case Qt::Key_Minus :
-        scale(1 / 1.2, 1 / 1.2);
-        break;
-    case Qt::Key_0 :
-        rotate(30);
-        break;
-
-    }
-    //    QGraphicsView::keyPressEvent(event);
-}
-*/
+/*****************
+*
+* key.c中接收自定义按键数据,
+* 发送keyEvent,在本函数中处理
+* 不用自定义事件是为了便于调试
+* 和增强可移植性
+*
+*****************/
 void PlView::keyPressEvent(QKeyEvent *event)
 {
     switch (event->key())
     {
 
-    case Qt::Key_Equal ://缩放
+    case Qt::Key_Equal ://缩放视野,坐标体系不变
         scale(1.2, 1.2);
         break;
     case Qt::Key_Minus :
@@ -201,46 +187,47 @@ void PlView::keyPressEvent(QKeyEvent *event)
 
     case Qt::Key_Up ://上下左右移动光标
         if(cursor.pos().y()> PlView::pos().y() )//防止光标越界
-            cursor.setPos((QPoint(cursor.pos().x(),cursor.pos().y()-10)));
+            cursor.setPos((QPoint(cursor.pos().x(),cursor.pos().y()-8)));
         break;
     case Qt::Key_Down :
         if(cursor.pos().y() < (PlView::pos().y() + PlView::height()) )
-            cursor.setPos((QPoint(cursor.pos().x(),cursor.pos().y()+10)));
+            cursor.setPos((QPoint(cursor.pos().x(),cursor.pos().y()+8)));
         break;
     case Qt::Key_Left :
         if(cursor.pos().x() > PlView::pos().x() )
-        {   cursor.setPos((QPoint(cursor.pos().x()-10,cursor.pos().y())));
+        {   cursor.setPos((QPoint(cursor.pos().x()-8,cursor.pos().y())));
             qDebug("Key_Left");
         }break;
     case Qt::Key_Right :
         if(cursor.pos().x() < (PlView::pos().x()+ PlView::width()))
-        {   cursor.setPos((QPoint(cursor.pos().x()+10,cursor.pos().y())));
+        {   cursor.setPos((QPoint(cursor.pos().x()+8,cursor.pos().y())));
             qDebug("Key_Right");
         }break;
 
 
-    case Qt::Key_Space ://管理途径点
-    {//若当前光标处有item，则删除
+    case Qt::Key_Space ://管理途径点,以16x16的正方形表示
+    {
         if(scene()->itemAt(QPoint(
-                               mapFromGlobal(QCursor::pos()).x()-10,
-                               mapFromGlobal(QCursor::pos()).y()-10)
+                               mapFromGlobal(QCursor::pos()).x()-8,
+                               mapFromGlobal(QCursor::pos()).y()-8)
                            )== 0)
-        {scene()->addRect(
-                        mapFromGlobal(QCursor::pos()).x()-10,
-                        mapFromGlobal(QCursor::pos()).y()-10,
-                        20,20);
-
+        {scene()->addRect(//若无则添加
+                        mapFromGlobal(QCursor::pos()).x()-8,
+                        mapFromGlobal(QCursor::pos()).y()-8,
+                        16,16);
+            qDebug("point:%d,%d",mapFromGlobal(QCursor::pos()).x(),mapFromGlobal(QCursor::pos()).y());
         }
-        //        {
+       /*
         //            XItem *item = new XItem;
         //            item->setPos(mapFromGlobal(QCursor::pos()).x()-10,
         //                         mapFromGlobal(QCursor::pos()).y()-10);
         //            scene()->addItem(item);
-        //        }
-        //自定义item不能被itemAt识别？
-        else scene()->removeItem(scene()->itemAt(QPoint(
-                                                     mapFromGlobal(QCursor::pos()).x()-10,
-                                                     mapFromGlobal(QCursor::pos()).y()-10)));
+        //自定义item不能被itemAt识别？无法正常删除
+        */
+        else {scene()->removeItem(scene()->itemAt(QPoint(//若当前光标处有item，则删除
+                                                     mapFromGlobal(QCursor::pos()).x()-8,
+                                                     mapFromGlobal(QCursor::pos()).y()-8)));
+        }
     }break;
     }
 }
